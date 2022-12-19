77c5ba26b11cc5e028a58655970bea2ed7961b61
Taweechai Yuenyang <krumii.it@gmail.com>
2022-12-19 13:32:03 +0700

    update

--- appservice/temperature.go
+++ appservice/temperature.go
@@ -14,6 +14,15 @@ func main() {
 		log.Fatal(err)
 	}
 	// Print temperature and humidity
-	fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
+	fmt.Printf("Pin 11 Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
+		temperature, humidity, retried)
+
+	temperature, humidity, retried, err =
+		dht.ReadDHTxxWithRetry(dht.DHT22, 17, false, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Print temperature and humidity
+	fmt.Printf("Pin 17 Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
 		temperature, humidity, retried)
 }
