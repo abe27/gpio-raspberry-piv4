18e2ff35e9fb4db11e3f3b4df5869ab2b0620afb
Taweechai Yuenyang <krumii.it@gmail.com>
2022-12-19 14:11:54 +0700

    new update

--- dht22/main.py
+++ dht22/main.py
@@ -5,20 +5,13 @@ import time
 
 
 #GPIO 21
-out = 11
+out = 17
 
 #c = celsius
 #f = Fahrenheit
 #k = kelvin
 units = 'c'
 sensor = DHT22.DHT22(out,units)
-time.sleep(1)
-running = True
-while(running):
-    try:
-        humidity, temperature = sensor.get_value()
-        if humidity is not None and temperature is not None:
-            print ('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-        time.sleep(2)
-    except KeyboardInterrupt:
-        running = False
+humidity, temperature = sensor.get_value()
+if humidity is not None and temperature is not None:
+    print ('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
