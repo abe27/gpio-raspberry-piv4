796e75bb6425ba02566cafd47b586651c131f54c
Taweechai Yuenyang <krumii.it@gmail.com>
2022-12-27 09:39:50 +0700

    update linechart

--- webapp/components/index.js
+++ webapp/components/index.js
@@ -1,6 +1,6 @@
 import DeviceComponent from "./Device";
 import LineTokenComponent from "./LineToken";
 import NotificationComponent from "./Notification";
-import LineChart from "./LineChart";
+// import LineChart from "./LineChart";
 
-export { DeviceComponent, LineTokenComponent, NotificationComponent, LineChart };
+export { DeviceComponent, LineTokenComponent, NotificationComponent };
